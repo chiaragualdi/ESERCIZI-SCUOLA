bf4f4638270574d44165d2aeeef62e2816716ade
chiaragualdi <73993515+chiaragualdi@users.noreply.github.com>
2020-12-23 08:58:50 +0100

    Update es30pag73.py

--- es30pag73.py
+++ es30pag73.py
@@ -1,3 +1,6 @@
+#Fornisci la rappresentazione in decimale di un numero binario.
+#Confronta il risultato con il valore ottenuto dalla funzione predefinita del linguaggio per convertire un numero binario in decimale.
+
 lunghezza=int(input("Inserisci il numero di cifre da cui è composto il numero binario: "))
 somma=0
 for i in range(lunghezza):
