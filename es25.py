af7adaeca60182b3fee2b6a80cfab227312b607f
chiaragualdi <73993515+chiaragualdi@users.noreply.github.com>
2020-12-23 08:38:12 +0100

    Update es25.py

--- es25.py
+++ es25.py
@@ -1,3 +1,5 @@
+#A un concorso pubblico hanno partecipato due candidati di cui si conoscono i nomi e i punteggi conseguiti.
+#Visualizza l'elenco dei due candidati prima in ordine alfabetico e poi in ordine decrescente di punteggio.
 candidato1=input("Inserisci il nome del primo candidato: ")
 voti1=int(input("Inserisci i voti che ha ottenuto: "))
 candidato2=input("Inserisci il nome del secondo candidato: ")
