2932ebf3d5d74a92fd68020b00f2e52fe815d479
chiaragualdi <73993515+chiaragualdi@users.noreply.github.com>
2020-12-23 08:35:45 +0100

    Update es24.py

--- es24.py
+++ es24.py
@@ -1,4 +1,5 @@
-
+#Alla fine della giornata di elezioni per il ballottaggio tra due candidati, si acquisiscono i voti ottenuti dai due candidati.
+#Scrivi il programma che calcoli le percentuali ottenute da ciascun candidato e comunichi il nome del vincitore.
 x=int(input("Inserisci il numero divoti ottenuti dal primo candidato"))
 y=int(input("Inserisci il numero di voti ottenuti dal secondo candidato"))
 tot=int(x+y)
